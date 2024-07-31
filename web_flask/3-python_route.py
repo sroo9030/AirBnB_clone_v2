e780223760a75e8c1c935f0c2c5af3d1b390fedd
lenovo <eisra3000ese@gmail.com>
2024-08-01 01:36:17 +0300

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Flask web application that listens on 0.0.0.0, port 5000.
+Provides four routes
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!' for the root route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays 'HBNB' for the /hbnb route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays 'C ' followed by the value of the text variable.
+    Replaces underscore _ symbols with a space.
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Displays 'Python ' followed by the value of the text variable.
+    Replaces underscore _ symbols with a space.
+    The default value of text is 'is cool'.
+    """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
