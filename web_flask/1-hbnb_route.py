529a81f7624de530dbdb439fe85fd5bb52b67ea1
lenovo <eisra3000ese@gmail.com>
2024-08-01 00:51:06 +0300

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,9 +5,8 @@ Provides two routes:
 """
 
 from flask import Flask
-
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -16,12 +15,14 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Displays 'HBNB' for the /hbnb route.
     """
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
