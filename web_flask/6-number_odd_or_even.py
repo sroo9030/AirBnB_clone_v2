aa0031f69181eaa520c37c175b0cf5c5db83940b
lenovo <eisra3000ese@gmail.com>
2024-08-01 01:53:14 +0300

    6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+Flask web application that listens on 0.0.0.0, port 5000.
+Provides several routes:
+7. /number_odd_or_even/<n>: displays a HTML page only if n is an integer,
+   with an H1 tag “Number: n is even|odd” inside the tag BODY.
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!' for the root route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays 'HBNB' for the /hbnb route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays 'C ' followed by the value of the text variable.
+    Replaces underscore _ symbols with a space.
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Displays 'Python ' followed by the value of the text variable.
+    Replaces underscore _ symbols with a space.
+    The default value of text is 'is cool'.
+    """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Displays 'n is a number' only if n is an integer.
+    """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Displays a HTML page only if n is an integer,
+    with an H1 tag 'Number: n' inside the tag BODY.
+    """
+    return render_template('number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Displays a HTML page only if n is an integer,
+    with an H1 tag 'Number: n is even|odd' inside the tag BODY.
+    """
+    if n % 2 == 0:
+        result = f"Number: {n} is even"
+    else:
+        result = f"Number: {n} is odd"
+    return render_template('number_odd_or_even.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
